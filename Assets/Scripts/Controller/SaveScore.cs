22cd4ab48bcabefb511ad4c53678d6c4b4e6c600
ltabis <lucas.tabis@epitech.eu>
2020-02-26 16:32:34 +0100

    :sparkles: Added a basic persistant score system.

---
+++ Assets/Scripts/Controller/SaveScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveScore : MonoBehaviour
+{
+    // Set the current score of the player.
+    void SetScore(int score)
+    {
+        PlayerPrefs.SetInt("Score", score);
+    }
+
+    // Set highscore of the player.
+    void SetHighScore(int score)
+    {
+        if (score > PlayerPrefs.GetInt("Highscore"))
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+        }
+    }
+}
