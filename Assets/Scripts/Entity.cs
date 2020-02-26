01b6e654653a46e2ad5ce44a052809ec2ffc7c8f
ltabis <lucas.tabis@epitech.eu>
2020-02-26 14:09:09 +0100

    :arrow_up: Added a basic movement system for a player.

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    protected float _velocity  = 1;
+    protected float _health    = 1;
+
+    // Get the velocity of the entity.
+    public float GetVelocity()
+    {
+        return _velocity;
+    }
+
+    // Get the health of the entity.
+    public float GetHealth()
+    {
+        return _health;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Entity
+{
+    public  Camera     playerCamera;
+    private Transform  cameraTransform;
+
+    void Start()
+    {
+        _velocity = 2;
+
+        cameraTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ComputeMovementControls();
+        // ComputeCameraControls();
+    }
+
+    // Moving the player with axis controls.
+    void ComputeMovementControls()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (x != 0 || y != 0)
+        {
+            transform.position += new Vector3(x * _velocity * Time.deltaTime, 0, y * _velocity * Time.deltaTime);
+        }
+    }
+
+    void ComputeCameraControls()
+    {
+        Vector3 cameraDirection = new Vector3(cameraTransform.forward.x, 0f, cameraTransform.forward.z);
+        Vector3 playerDirection = new Vector3(_velocity, 0f, _velocity);
+
+        if (Vector3.Angle(cameraDirection, playerDirection) > 15f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation, 15f);
+        }
+    }
+}
